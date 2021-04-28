ee4493f56d1a05273ea6dfa0e86d4c17be9635df
Victor Hugo Molinar <vhmolinar@gmail.com>
2021-04-27 23:22:37 -0300

    Implementing chapter 02

---
+++ chapter-02/guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use colored::*;
+
+fn main() {
+  println!("Guess the number!");
+
+  let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+
+  loop {
+    println!("Please input your guess.");
+    let mut guess: String = String::new();
+
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Type a valid number");
+        continue;
+      }
+    };
+
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("{}", "Too small".red()),
+      Ordering::Greater => println!("{}", "Too big".red()),
+      Ordering::Equal => {
+        println!("{}", "Bingo".green());
+        break;
+      },
+    }
+  }
+
+}
